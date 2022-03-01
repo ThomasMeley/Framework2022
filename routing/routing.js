291311d2eb15dad3d826e59713d1e7f949e0ed9c
Thomt <thomtails.Meley@gmail.Com>
2022-03-01 16:03:45 +0100

    ajout début

--- routing/routing.js
+++ routing/routing.js
@@ -1,18 +1,32 @@
-import {
+import { createRouter, createWebHistory } from "vue-router";
+// Ici, on importe les composants qu'on veut configurer
+// Dans ce cas, ce sera Accueil et Details
+import Accueil from "./Accueil.vue";
+import Details from "./Details.vue";
 
-createRouter
-createWebhistory
-}
-from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import About from '../views/About.vue'
+// Nous créons d'abord nos Routes
+const routes = [
+  // En n'indiquant rien d'autre que '/' dans path
+  // Nous disons à Vue que c'est la page racine
+  // Donc, la racine de votre application
+  // Affichera le composant "Home.vue"
+  {
+    path: "/",
+    component: Home,
+  },
+  // Ici, nous disons que pour la route "/details"
+  // Nous afficherons le composant "Details.vue"
+  {
+    path: "/details",
+    component: Details,
+  },
+];
 
-const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes : [
-    {path: '/', name: 'home',component: HomeView},
-    {path : '/about', name: 'about',compenent : About}
-    ]
-})
-
-export default
+// On crée notre router à partir des routes configurées
+export default createRouter({
+  // Afin que votre Router puisse fonctionner correctement
+  // Nous avons besoin de lui créer un système d'historique
+  history: createWebHistory(),
+  // Nous ajoutons les routes fraîchement configurées
+  routes,
+});
