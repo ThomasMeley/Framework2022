219b768db194b2c75c47648952c16173da200f31
Thomt <thomtails.Meley@gmail.Com>
2022-03-01 17:19:34 +0100

    commit massif

---
+++ TP2/Form.vue
@@ -0,0 +1,39 @@
+</template>
+<script>
+import {defineComponent} from "vue";
+import axios from "axios";
+
+export default defineComponent({
+
+    data:()=>{
+        return {
+            nom: "",
+            prenom :"",
+            date:"",
+            email:"",
+            isFormActive: true,
+            tweets:[],
+
+        };
+    },
+
+    monted() {
+        axios.get("https://fges-twitter-clone.herokuapp.com/allTweets", {
+            header:{
+            "x-fges-user-key": "VOTRE CLE",
+        },
+    }).then((resultats)=>{
+        //gerer vos resultats ici
+        console.log(resultats.data) }); 
+    })
+        )
+    methods : {
+        hideForm() {
+            if (this.nom && this.person && this.Date && this.email){
+
+                
+            }
+        }
+    }
+    }
+})
---
+++ exercise.js
@@ -0,0 +1,39 @@
+const { createApp } = require('vue');
+import App from "./App.vue";
+import {defineComponent};
+
+
+createApp(App).mount("#app");{
+
+<template>
+
+<lacalculette button ="1"/>
+<lacalculette button ="2"/>
+<lacalculette button ="3"/>
+<lacalculette button ="+"/>
+<br>
+<lacalculette button ="4"/>
+<lacalculette button ="5"/>
+<lacalculette button ="6"/>
+<lacalculette button ="-"/>
+<br>
+<lacalculette button ="7"/>
+<lacalculette button ="8"/>
+<lacalculette button ="9"/>
+<lacalculette button ="*"/>
+<br>
+<lacalculette button ="0"/>
+<lacalculette button ="/"/>
+
+</template>
+
+
+import {defineComponent}from vue ;
+<script>
+
+function lacalculette () {
+
+    
+}
+
+</script>
